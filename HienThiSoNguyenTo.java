38bfec5a8267f77803a955faa263a7aef478fd77
mrha140799 <mrha140799@gmail.com>
2019-02-22 10:00:05 +0700

    [Bài tập] Hiển thị các số nguyên tố nhỏ hơn 100

---
+++ HienThiSoNguyenTo.java
@@ -0,0 +1,27 @@
+package SS1BT3;
+
+public class HienThiSoNguyenTo {
+    public static void main(String[] args) {
+        int dem = 3;
+        for (int i = 2 ;i < 100 ;i++) {
+
+            if (isPrime(i)){
+                System.out.print(i +", ");
+                dem++;
+            }
+        }
+    }
+
+    public static boolean isPrime (int data) {
+        int dem =0;
+        for (int i = 2 ; i <= Math.sqrt(data) ; i++) {
+            if (data % i ==0) {
+                dem++;
+            }
+        }
+        if (dem == 0) {
+            return true;
+        }
+        return false;
+    }
+}
